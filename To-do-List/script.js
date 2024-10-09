0096ecff035ead942c19d9a6d510ead35613ede9
Valeria Mikhalevich <valeerriam@gmail.com>
2024-10-09 22:26:05 +0300

    To-Do List

---
+++ To-do-List/script.js
@@ -0,0 +1,51 @@
+const inputField = document.querySelector(".input-field textarea"),
+  toDoList = document.querySelector(".toDoList"),
+  pendingNum = document.querySelector(".pending-num"),
+  clearButton = document.querySelector(".clear-button");
+
+function allTasks() {
+  let tasks = document.querySelectorAll(".pending");
+  pendingNum.textContent = tasks.length === 0 ? "no" : tasks.length;
+
+  let allLists = document.querySelectorAll(".list");
+  if (allLists.length > 0) {
+    toDoList.style.marginTop = "20px";
+    clearButton.style.pointerEvents = "auto";
+    return;
+  }
+  toDoList.style.marginTop = "0px";
+  clearButton.style.pointerEvents = "none";
+}
+
+inputField.addEventListener("keyup", (e) => {
+  let inputVal = inputField.value.trim();
+
+  if (e.key === "Enter" && inputVal.length > 0) {
+    let liTag = `<li class="list pending" onclick="handleStatus(this)">
+          <input type="checkbox" />
+          <span class="task">${inputVal}</span>
+          <i class="uil uil-trash" onclick="deleteTask(this)";></i>
+        </li>`;
+
+    toDoList.insertAdjacentHTML("beforeend", liTag);
+    inputField.value = " ";
+    allTasks();
+  }
+});
+
+function handleStatus(e) {
+  const checkbox = e.querySelector("input");
+  checkbox.checked = checkbox.checked ? false : true;
+  e.classList.toggle("pending");
+  allTasks();
+}
+
+function deleteTask(e) {
+  e.parentElement.remove();
+  allTasks();
+}
+
+clearButton.addEventListener("click", () => {
+  toDoList.innerHTML = "";
+  allTasks();
+});
